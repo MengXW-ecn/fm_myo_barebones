8adf21557b7d73e063495fe0059debc0e16d2c2c
MengXW-ecn <80529653+MengXW-ecn@users.noreply.github.com>
2021-05-17 01:50:00 +0200

    Create listenersPlus.py

---
+++ myo_ecn/listenersPlus.py
@@ -0,0 +1,51 @@
+from collections import deque
+from threading import Lock, Thread
+import numpy as np
+import time
+import myo
+#from IIRFilter import LowPassIIR
+
+
+
+class BufferPlus(myo.DeviceListener):
+    #An instance of this class constantly collects new EMG data in a queue (buffer)
+    def __init__(self, buffer_len):
+        self.n = buffer_len
+        self.lock = Lock()
+        self.mav_data_queue = deque(maxlen=self.n)
+        # self.y = 0
+        # self.a = 24/25*np.ones([8,1])
+        self.y = np.zeros(8)
+        self.a = 24/25
+        self.mav_data_queue = deque(maxlen=self.n)
+
+    def filter(self,x):
+        # self.y = (1-self.a[self.i]) * x + self.a[self.i] * self.y
+        self.y = (1 - self.a) * x + self.a * self.y
+        return self.y
+
+    # def get_mav_data(self,in_data):
+        # mav_data = []
+        # #num_splitarray = np.linspace(0,496,64,dtype=int) #(step = 7, num = 64)
+
+        # with self.lock:
+            # # compute the MAV data
+            # #for j in num_splitarray:
+            # for self.i in range(0,8):
+
+                # col_data = in_data[:,self.i]
+                # abs_data = np.absolute(col_data)
+                
+                # # filter
+                # for n in range(0, len(abs_data)):
+                    # abs_data[n] = self.filter(abs_data[n])
+                # mav_data.append(list(abs_data))
+
+        # return mav_data
+        
+    def get_mav_data(self, in_data):  # 512*8
+        mav_data = np.zeros((0,8))  # 8 columns
+        for sample in in_data:
+            aaa = self.filter(np.array(abs(sample)))
+            mav_data = np.row_stack((mav_data, aaa))
+        return mav_data
