68f4a4dd381e9c130bf907774d0c806e0832ceca
MengXW-ecn <80529653+MengXW-ecn@users.noreply.github.com>
2021-05-17 02:20:36 +0200

    Update mav.py

--- mav.py
+++ mav.py
@@ -10,8 +10,6 @@ from MultichannelPlot                    import MultichannelPlot
 from myo_ecn.listenersPlus               import BufferPlus
 
 
-
-
 def main():
     # ================== setup myo-python (do not change) =====================
     myo.init(sdk_path='../../myo_sdk') # Compile Python binding to Myo's API
@@ -26,8 +24,7 @@ def main():
     computer = BufferPlus(buffer_len = 512)
     # Setup multichannel plotter for visualisation:
     plotter = MultichannelPlot(nchan = 8, xlen = 512) # Number of EMG channels in MYO armband is 8 , window size is 15 for MAV
-    freq = 200
-    move = cursor(freq)
+
     # Tell MYO API to satart a parallel thread that will collect the data and
     # command the MYO to start sending EMG data.
     with hub.run_in_background(listener): # This is the way to associate our listener with the MYO API.
@@ -47,7 +44,7 @@ def main():
                     # update data
                     plotter.update_plot(np.array(mav_data))
                     
-                    move.move_cursor(mav_data)
+           
             
             if keyboard.is_pressed('C'):
                 print('Stop.')
@@ -56,3 +53,41 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+
+    
+    
+
+# ---------------------
+class Moving_Average(object):
+    def __init__(self, length, return_int = False):
+        self.data = []
+        self.data_sum = -1
+        #self.data_avg = -1
+        self.length = length
+        self.value = -1
+        self.return_int = return_int
+
+        #self.sample_frequency = sample_frequency               #in Hz
+        #self.range_ = range_                                   #in seconds
+        #self.scope = 1.0 * self.sample_frequency * self.range_       #in number of samples, limits the length of movingAvg    
+        #self.sum_movingAvg = 0                                 #tracks the sum of the moving average
+        #self.val_movingAvg = -1                                #the latest moving average value
+        #self.movingAvg = []                                    #used to store the datapoints for taking a moving average
+
+    def get_movingAvg (self, data):
+        self.data.insert(0, data)
+        self.data_sum += data
+
+        if len(self.data) > self.length:
+            self.data_sum -= self.data.pop()
+
+        if self.return_int == True:
+            self.value = int(self.data_sum / self.length) #preserves integer form
+        else: 
+            self.value = 1.0 * self.data_sum / self.length
+
+        if len(self.data) < (self.length / 2):
+            return -1
+        else:
+            return self.value
