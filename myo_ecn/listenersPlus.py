2f69faf8f11faec0a5b7825bbd2e59984b83dc91
MengXW-ecn <80529653+MengXW-ecn@users.noreply.github.com>
2021-05-17 02:29:05 +0200

    Update listenersPlus.py

--- myo_ecn/listenersPlus.py
+++ myo_ecn/listenersPlus.py
@@ -13,39 +13,28 @@ class BufferPlus(myo.DeviceListener):
         self.n = buffer_len
         self.lock = Lock()
         self.mav_data_queue = deque(maxlen=self.n)
-        # self.y = 0
-        # self.a = 24/25*np.ones([8,1])
-        self.y = np.zeros(8)
-        self.a = 24/25
-        self.mav_data_queue = deque(maxlen=self.n)
+        self.y = 0
+        self.a = 24/25*np.ones([8,1])
 
     def filter(self,x):
-        # self.y = (1-self.a[self.i]) * x + self.a[self.i] * self.y
-        self.y = (1 - self.a) * x + self.a * self.y
+        self.y = (1-self.a[self.i]) * x + self.a[self.i] * self.y
         return self.y
 
-    # def get_mav_data(self,in_data):
-        # mav_data = []
-        # #num_splitarray = np.linspace(0,496,64,dtype=int) #(step = 7, num = 64)
+    def get_mav_data(self,in_data):
+        mav_data = []
+        #num_splitarray = np.linspace(0,496,64,dtype=int) #(step = 7, num = 64)
 
-        # with self.lock:
-            # # compute the MAV data
-            # #for j in num_splitarray:
-            # for self.i in range(0,8):
+        with self.lock:
+            # compute the MAV data
+            #for j in num_splitarray:
+            for self.i in range(0,8):
 
-                # col_data = in_data[:,self.i]
-                # abs_data = np.absolute(col_data)
+                col_data = in_data[:,self.i]
+                abs_data = np.absolute(col_data)
                 
-                # # filter
-                # for n in range(0, len(abs_data)):
-                    # abs_data[n] = self.filter(abs_data[n])
-                # mav_data.append(list(abs_data))
+                # filter
+                for n in range(0, len(abs_data)):
+                    abs_data[n] = self.filter(abs_data[n])
+                mav_data.append(list(abs_data))
 
-        # return mav_data
-        
-    def get_mav_data(self, in_data):  # 512*8
-        mav_data = np.zeros((0,8))  # 8 columns
-        for sample in in_data:
-            aaa = self.filter(np.array(abs(sample)))
-            mav_data = np.row_stack((mav_data, aaa))
         return mav_data
